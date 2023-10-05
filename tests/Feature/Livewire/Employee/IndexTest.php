510ece39a1dbefa90e48f3d9fbe58e4c6af0b7a4
JamesLilliott <jameslilliott1997@gmail.com>
2023-10-05 20:52:52 +0100

    Add sorting test

--- tests/Feature/Livewire/Employee/IndexTest.php
+++ tests/Feature/Livewire/Employee/IndexTest.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Livewire\Employee\Index;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Livewire\Livewire;
 
 it('renders successfully', function () {
@@ -22,11 +21,39 @@ it('sees an empty table', function () {
         ->assertSee('No data found.');
 });
 
-it('see page 1 of employee table', function () {
+it('don\'t see no data found', function () {
     $this->actingAs(\App\Models\User::factory()->create());
 
     \App\Models\Employee::factory(2)->create();
 
     Livewire::test(Index::class)
         ->assertDontSee('No data found.');
+});
+
+it('can see pagination', function () {
+    $this->actingAs(\App\Models\User::factory()->create());
+
+    \App\Models\Employee::factory(4)->create();
+
+    Livewire::test(Index::class)
+        ->assertSee('paginator-page-page1');
+});
+
+it('can sort columns', function () {
+    \Illuminate\Support\Facades\DB::table('employees')->truncate();
+    $this->actingAs(\App\Models\User::factory()->create());
+
+    \App\Models\Employee::factory()->create(['name' => 'Alice']);
+    \App\Models\Employee::factory()->create(['name' => 'Bob']);
+    \App\Models\Employee::factory()->create(['name' => 'Charlie']);
+
+    Livewire::test(Index::class)
+        ->call('sort', 'name')
+        ->assertSee('Charlie')
+        ->assertSee('Bob')
+        ->assertDontSee('Alice')
+        ->call('sort', 'name')
+        ->assertSee('Alice')
+        ->assertSee('Bob')
+        ->assertDontSee('Charlie');
 });
